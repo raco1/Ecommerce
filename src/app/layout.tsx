57a14580eb528a96fd72b3b545a6f551089f246f
rafael <rafaelcoelho2711@gmail.com>
2024-02-23 09:35:49 -0300

    atualiza o layout root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,20 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter'})
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Devstore',
 }
 
-export default function RootLayout({children,}: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html className={inter.variable} lang="pt-br">
-      <body className='bg-zinc-950 text-zinc-50 antialiased'>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
