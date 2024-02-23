9d8e48d994a375d689eb9efb2b9e5321c8364ba5
rafael <rafaelcoelho2711@gmail.com>
2024-02-23 09:36:12 -0300

    cria o layout da loja

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,12 @@
-import {ReactNode} from "react"
+import { ReactNode } from 'react'
 
-import { Header } from "@/components/header"
+import { Header } from '@/components/header'
 
-export default function StoreLayout({children}: {children: ReactNode}){
-    return (
-        <div className='mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8'>
-        <Header />
-        { children }
-        </div>
-    )
+export default function StoreLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+      <Header />
+      {children}
+    </div>
+  )
 }
