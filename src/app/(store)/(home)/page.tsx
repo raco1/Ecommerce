6ee2ffbf1dbe0e46e14634e4eb1ec9aa841b9333
rafael <rafaelcoelho2711@gmail.com>
2024-07-04 11:11:14 -0300

    page metadata no next

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -2,6 +2,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api('/products/featured', {
--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 
 interface ProductProps {
@@ -18,6 +19,15 @@ async function getProducts(slug: string): Promise<Product> {
   const products = await response.json()
 
   return products
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProducts(params.slug)
+  return {
+    title: product.title,
+  }
 }
 
 export default async function ProductPage({ params }: ProductProps) {
